c598d10362d78edbd1bd8b9b1cc3181a137914a4
yuanqing <hello@yuanqing.sg>
2015-04-13 03:04:40 +0800

    Use a closure to persist the Hashtbl

--- fibonacci/fibonacci.ml
+++ fibonacci/fibonacci.ml
@@ -1,23 +1,23 @@
 open Big_int
 
-let ht = Hashtbl.create 100
-
-let _ = Hashtbl.add ht 1 (big_int_of_int 0)
-let _ = Hashtbl.add ht 2 (big_int_of_int 1)
-
-let fib (n:int) : big_int =
-  if n < 1 then
-    failwith "n < 1"
-  else
-    try
-      Hashtbl.find ht n
-    with Not_found ->
-      let rec aux i n =
-        let x = add_big_int (Hashtbl.find ht (i - 1))
-            (Hashtbl.find ht (i - 2)) in
+let fib =
+  let ht = ref (Hashtbl.create 100) in
+  let _ = Hashtbl.add (!ht) 1 (big_int_of_int 0) in
+  let _ = Hashtbl.add (!ht) 2 (big_int_of_int 1) in
+  fun (n:int) ->
+    let ht = !ht in
+    if n < 1 then
+      failwith "n < 1"
+    else
+      try
+        Hashtbl.find ht n
+      with Not_found ->
+        let rec aux i n =
+          let x = add_big_int (Hashtbl.find ht (i - 1))
+              (Hashtbl.find ht (i - 2)) in
           if i = n then
             x
           else
             let () = Hashtbl.add ht i x in
-            aux (i + 1) n
-      in (aux 3 n)
+            aux (i + 1) n in
+        (aux 3 n);;
