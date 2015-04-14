a446c7b7203b1aded8c679df5145711164859f54
yuanqing <hello@yuanqing.sg>
2015-04-14 23:25:01 +0800

    Edit comments

--- insertion_sort/insertion_sort.ml
+++ insertion_sort/insertion_sort.ml
@@ -2,12 +2,14 @@ let insertion_sort (comp:'a -> 'a -> int) (xs:'a list) : 'a list =
   let rec insert comp x ys =
     match ys with
       | [] -> [x]
-      | y::ys as zs when (comp x y < 0) -> x::zs
-        (* Insert `x` before `zs` if the comparator function returns a
-           negative value. *)
-      | y::ys -> y::(insert comp x ys) in
+      | y::ys as zs when (comp x y < 0) ->
+        (* `comp` returning a negative value means that `x` is less than
+           `y`. So we place `x` in front of both `y` and `ys`. *)
+        x::zs
+      | y::ys ->
         (* Otherwise, continue looking for a place to insert `x` in the
-           remainder of the list. *)
+           rest of the list. *)
+        y::(insert comp x ys) in
   let rec aux xs =
     match xs with
       | [] -> []
