91d059a8a76a1d61a44f95bfd6df57a00d1176a7
yuanqing <hello@yuanqing.sg>
2015-04-15 02:48:10 +0800

    Initial commit

---
+++ merge_sort/merge_sort.ml
@@ -0,0 +1,32 @@
+let merge_sort (comp:'a -> 'a -> int) (xs:'a list) : 'a list =
+  let rec merge xs ys =
+    match xs, ys with
+      | xs, [] -> xs
+      | [], ys -> ys
+      | x::xs, y::_ when comp x y < 0 ->
+        (* `comp` returning a negative value means that `x` is less than `y`.
+           So prepend `x` to the result of merging the rest of the two
+           lists. *)
+        x::(merge xs ys)
+      | xs, y::ys ->
+        (* Otherwise, prepend `y` to the result of merging the rest
+           of the two lists. *)
+        y::(merge xs ys) in
+  let split xs =
+    (* Split `xs` into two halves. *)
+    let len = List.length xs / 2 in
+    let rec aux xs acc i =
+      match xs with
+        | x::xs when i < len -> aux xs (x::acc) (i + 1)
+        | _ -> List.rev acc, xs in
+    aux xs [] 0 in
+  let rec aux xs =
+    match xs with
+      | [] -> []
+      | [x] -> xs
+      | _ ->
+        (* Split `xs` into two halves, calling `aux` on each half and merging
+           the two halves. *)
+        let ys, zs = split xs in
+        merge (aux ys) (aux zs) in
+  aux xs
---
+++ merge_sort/test.ml
@@ -0,0 +1,34 @@
+open Merge_sort
+open OUnit2
+
+let asc x y =
+  if x < y then -1
+  else if x > y then 1
+  else 0
+
+let dsc x y =
+  if x < y then 1
+  else if x > y then -1
+  else 0
+
+let () = run_test_tt_main ("merge_sort comp xs" >::: [
+
+  "empty list" >:: (fun _ ->
+    assert_equal [] (merge_sort asc [])
+  );
+
+  "single item" >:: (fun _ ->
+    assert_equal [42] (merge_sort asc [42])
+  );
+
+  "multiple items; ascending order" >:: (fun _ ->
+    assert_equal [4; 8; 15; 16; 23; 42]
+        (merge_sort asc [42; 8; 15; 23; 4; 16])
+  );
+
+  "multiple items; descending order" >:: (fun _ ->
+    assert_equal [42; 23; 16; 15; 8; 4]
+        (merge_sort dsc [42; 8; 15; 23; 4; 16])
+  );
+
+])
