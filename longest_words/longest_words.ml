8e9e33e51d98561bbf9913c763118ad4967403d2
yuanqing <hello@yuanqing.sg>
2015-04-20 21:12:13 +0800

    Remove `ref` type

--- longest_words/longest_words.ml
+++ longest_words/longest_words.ml
@@ -2,27 +2,27 @@ let longest_words (sentence:string) : string list =
   (* Split `sentence` on whitespace.  *)
   let regexp = Str.regexp "[ ]+" in
   let words = Str.split regexp sentence in
-  (* Initialise `ht` and `max`. *)
+  (* Initialise `ht`. *)
   let ht = Hashtbl.create (List.length words) in
-  let max = ref (-1) in
-  (* Iterate over each word, keeping track of the longest words. *)
-  let fn word =
-    let len = String.length word in
-    let max_val = !max in
-    if len >= max_val then
-      let () =
-        if len > max_val then
+  let rec aux xs max : string list =
+    match xs with
+      | [] ->
+        let fn _ word acc =
+          word::acc in
+        Hashtbl.fold fn ht []
+      | x::xs ->
+        let len = String.length x in
+        if len >= max then
           (* If the `word` is longer than the current `max`, clear the `ht` and
              update the `max`. *)
-          let _ = Hashtbl.clear ht in
-          max := len in
-      (* Add `word` into `ht` only if it's lowercase representation doesn't
-         already exist in `ht`. *)
-      let lowercase_word = String.lowercase word in
-      if Hashtbl.mem ht lowercase_word = false then
-        Hashtbl.add ht lowercase_word word in
-  let () = List.iter fn words in
-  (* Return the longest words as a list. *)
-  let fn _ word acc =
-    word::acc in
-  Hashtbl.fold fn ht []
+          let () = if len > max then
+            Hashtbl.clear ht in
+          (* Add `word` into `ht` only if it's lowercase representation doesn't
+             already exist in `ht`. *)
+          let lowercase_word = String.lowercase x in
+          let () = if not (Hashtbl.mem ht lowercase_word) then
+            Hashtbl.add ht lowercase_word x in
+          aux xs (if len > max then len else max)
+        else
+          aux xs max in
+  aux words (-1)
