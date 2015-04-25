8fd505e3b87ea4e555d7fce3e37c76061b67b9af
yuanqing <hello@yuanqing.sg>
2015-04-25 12:23:00 +0800

    Add in-order traversal + tests to tree_traversal

---
+++ tree_traversal/test.ml
@@ -0,0 +1,60 @@
+open Tree_traversal
+open OUnit2
+
+let x =
+  Node (0,
+    Node (1,
+      Node (3,
+        Empty,
+        Empty
+      ),
+      Node (4,
+        Empty,
+        Node (7,
+          Empty,
+          Empty
+        )
+      )
+    ),
+    Node (2,
+      Node (5,
+        Node (8,
+          Empty,
+          Empty
+        ),
+        Empty
+      ),
+      Node (6,
+        Node (9,
+          Empty,
+          Empty
+        ),
+        Node (10,
+          Empty,
+          Empty
+        )
+      )
+    )
+  )
+
+let () = run_test_tt_main ("tree traversal" >::: [
+
+  "pre_order traversal" >:: (fun _ ->
+    let xs = [ 0; 1; 3; 4; 7; 2; 5; 8; 6; 9; 10 ] in
+    assert_equal xs (naive_pre_order x);
+    assert_equal xs (pre_order x)
+  );
+
+  "post-order traversal" >:: (fun _ ->
+    let xs = [ 3; 7; 4; 1; 8; 5; 9; 10; 6; 2; 0 ] in
+    assert_equal xs (naive_post_order x);
+    assert_equal xs (post_order x)
+  );
+
+  "in-order traversal" >:: (fun _ ->
+    let xs = [ 3; 1; 4; 7; 0; 8; 5; 2; 9; 6; 10 ] in
+    assert_equal xs (naive_in_order x);
+    assert_equal xs (in_order x)
+  );
+
+])
--- tree_traversal/tree_traversal.ml
+++ tree_traversal/tree_traversal.ml
@@ -2,28 +2,76 @@ type 'a tree =
   | Empty
   | Node of 'a * 'a tree * 'a tree
 
+let naive_pre_order (x:'a tree) : 'a list =
+  let rec aux x =
+    match x with
+      | Empty -> []
+      | Node (v, l, r) -> v::(aux l)@(aux r) in
+  aux x
+
 let pre_order (x:'a tree) : 'a list =
   let rec aux l rs =
-    (* Accumulate the right children in `rs` to allow tail recursion. *)
     match l with
       | Empty ->
+        (* If the current node is empty, traverse its sibling (ie. the parent
+        node's right child). *)
         begin match rs with
           | [] -> []
           | r::rs -> aux r rs
         end
-      | Node (v, l, r) -> v::(aux l (r::rs)) in
+      | Node (v, l, r) ->
+        (* Prepend `v`, the value of the current node to the result. Then
+        traverse its left child, accumulating its right child in `rs` to allow
+        tail recursion. *)
+        v::(aux l (r::rs)) in
   aux x []
+
+let naive_post_order (x:'a tree) : 'a list =
+  let rec aux x =
+    match x with
+      | Empty -> []
+      | Node (v, l, r) -> (aux l)@(aux r)@[v] in
+  aux x
 
 let post_order (x:'a tree) : 'a list =
   let rec aux r ls vs =
-    (* Accumulate the left children in `ls` to allow tail recursion. We also
-    use `vs` to accumulate values to avoid having to do a `List.rev` on
-    the final result. *)
     match r with
       | Empty ->
+        (* If the current node is empty, traverse its sibling (ie. the parent
+        node's left child). *)
         begin match ls with
           | [] -> vs
           | l::ls -> aux l ls vs
         end
+        (* Traverse the current node's right child, accumulating its left
+        child in `ls` and its value in `vs` to allow tail recursion. *)
       | Node (v, l, r) -> aux r (l::ls) (v::vs) in
+  aux x [] []
+
+let naive_in_order (x:'a tree) : 'a list =
+  let rec aux x =
+    match x with
+      | Empty -> []
+      | Node (v, l, r) -> (aux l)@(v::(aux r)) in
+  aux x
+
+let in_order (x:'a tree) : 'a list =
+  let rec aux l rs vs =
+    match l with
+      | Empty ->
+        (* If the current node is empty, prepend `v`, the value of its parent
+        node to the result, then traverse its sibling (ie. the parent node's
+        right child). *)
+        begin match rs with
+          | [] -> []
+          | r::rs ->
+            begin match vs with
+              | [] -> []
+              | v::vs -> v::(aux r rs vs)
+            end
+        end
+      | Node (v, l, r) ->
+        (* Traverse the left child. Accumulate the right children in `rs` and
+        the value of the current node in `vs`. *)
+        aux l (r::rs) (v::vs) in
   aux x [] []
