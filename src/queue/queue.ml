914e59ce69625d2d370504c51e995bbd2cdef5e8
yuanqing <desk@yuanqing.sg>
2015-06-01 21:50:41 +0800

    Make `enqueue` tail recursive

--- src/queue/queue.ml
+++ src/queue/queue.ml
@@ -5,7 +5,11 @@ type 'a queue = ('a list) ref
 let create () : 'a queue = ref []
 
 let enqueue (x:'a) (s:'a queue) : unit =
-  s := (!s)@[x]
+  let rec aux s =
+    match s with
+      | [] -> [x]
+      | x::xs -> x::(aux xs) in
+  s := aux !s
 
 let poll (s:'a queue) : 'a =
   match (!s) with
